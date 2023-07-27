7d596ce0d137d0c461bc901edba881e8cd97bc52
Heber Marcano <Hebelioo@gmail.com>
2023-07-27 09:53:50 -0400

    added code back

--- server.js
+++ server.js
@@ -1,38 +1,61 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
-const uniqid = require("uniqid")
-
-const PORT = process.env.PORT || 3002;
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname,"./public/index.html"))
-} )
-app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname,"./public/notes.html"))
-} )
+const PORT = process.env.PORT || 3002;
 
-app.get("/api/notes",(req,res) => {
-    fs.readFile("./db/db.json",(err,results) => {
-        let notes;
-        err ? console.log(err) : notes = JSON.parse(results)
-        console.log(notes)
-        res.json(notes)
-    })
-})
-app.post("/api/notes",(req,res) => {
-console.log(req.body)
-req.body.id = uniqid()
-console.log(req.body)
-fs.readFile(".")
-})
+app.use(express.json());
 
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      const notes = JSON.parse(data);
+      const newNote = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text,
+      };
+      notes.push(newNote);
+      fs.writeFile("./db.json", JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          res.sendStatus(500);
+        } else {
+          const updatedNotes = JSON.parse(JSON.stringify(notes));
+          res.json(updatedNotes);
+        }
+      });
+    }
+  });
+});
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`app listening at http://localhost:${PORT}`)
 );
